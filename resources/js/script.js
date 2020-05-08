7f0644465e6e05b760fafee2fc40b38c9658eab6
unknown <vadiksimf88@gmail.com>
2020-05-08 17:51:47 +0300

    Add burger-menu

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() { // Код запускается только когда страница загружена полностью
 
-
-    $('.js--section-features').waypoint(function(direction){
+    /*  Меню навигации  */
+    $('.js--section-features').waypoint(function(direction){ // При waypoint действие происходит при пересечении выбранной точки на веб-странице
         if (direction == "down") {
             $('nav').addClass('sticky');
         } else {
@@ -11,12 +11,96 @@ $(document).ready(function() { // Код запускается только к�
         offset: '60px;'
     })
 
-    /*
-    var waypoints = $('#handler-first').waypoint(function(direction) {
-  notify(this.element.id + ' hit 25% from top of window') 
-}, {
-  offset: '25%'
-})
-*/
+    /* Премещение с кнопок в Хэдере */ 
+    $('.js--scroll-to-plans').click(function() {
+        $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000) // Скролл в top нужного нам класса со скоростью 1000 мс
+    })
 
-})
+    /* Премещение с кнопок в Преимущества */ 
+    $('.js--scroll-to-features').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
+    })
+
+    /* Скролл для навигации */
+
+    // Select all links with hashes
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+    // On-page links
+    if (
+        location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+        && 
+        location.hostname == this.hostname
+    ) {
+        // Figure out element to scroll to
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        // Does a scroll target exist?
+        if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000, function() {
+            // Callback after animation
+            // Must change focus!
+            var $target = $(target);
+            $target.focus();
+            if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+            } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+            };
+        });
+        }
+    }
+    });
+
+    /* Анимация при скролле */ 
+
+    $('.js--wp-1').waypoint(function() {
+        $('.js--wp-1').addClass('animate__animated animate__fadeIn')
+    }, {
+        offset: '50%;'
+    })
+
+    $('.js--wp-2').waypoint(function() {
+        $('.js--wp-2').addClass('animate__animated animate__fadeInUp')
+    }, {
+        offset: '50%;'
+    })
+
+    $('.js--wp-3').waypoint(function() {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn')
+    }, {
+        offset: '50%;'
+    })
+
+    $('.js--wp-4').waypoint(function() {
+        $('.js--wp-4').addClass('animate__animated animate__pulse')
+    }, {
+        offset: '50%;'
+    })
+
+    /* Создание бургера */
+
+    $('.js--nav-burger').click(function () {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-burger i');
+
+        nav.slideToggle(200)
+
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.removeClass('ion-navicon-round');
+            icon.addClass('ion-close-round');
+        } else {
+            icon.removeClass('ion-close-round');
+            icon.addClass('ion-navicon-round');
+        }
+    });
+
+});
